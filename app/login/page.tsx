5c9e35e063f3b7e6408bfdafa3b92e6d54c6bd0b
hoangthien2003 <hoangdangbaothien@gmail.com>
2024-01-11 23:31:57 +0700

    fixed error coding convention

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Metadata } from 'next';
 
-export const metadat: Metadata = {
+export const metadata: Metadata = {
   title: 'Login',
 };
 
